5cc29a8107ca62a083ba22c999cabff53545133c
Jaroslaw Drapala <J.Drapala@kainos.com>
2014-12-06 17:15:22 +0000

    removed arguments from connect method.

--- soft/library/Soft/Db/Driver/Driver.php
+++ soft/library/Soft/Db/Driver/Driver.php
@@ -7,14 +7,9 @@ interface Driver
   /**
      * Attempts to create a connection with the database.
      *
-     * @param array       $params        All connection parameters passed by the user.
-     * @param string|null $username      The username to use when connecting.
-     * @param string|null $password      The password to use when connecting.
-     * @param array       $driverOptions The driver options to use when connecting.
-     *
      * @return \Soft\Db\Driver\Connection The database connection.
      */
-    public function connect(array $params, $username = null, $password = null, array $driverOptions = array());
+    public function connect();
 
 
     /**
