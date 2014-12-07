860a5368c082ee6f28a43adac7f1a6947c761269
gitcode-corp <gitcode.corp@gmail.com>
2014-12-07 21:35:22 +0000

    Created StatusCode class.

---
+++ library/Soft/Http/StatusCode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Soft\Http;
+
+class StatusCode
+{
+    const OK = 200;
+    const CREATED = 201;
+    const NO_CONTENT = 204;
+    const UNAUTHORIZED = 401;
+    const FORBIDDEN = 403;
+    const NOT_FOUND = 404;
+    
+}
+
